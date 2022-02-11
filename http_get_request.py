6d8a78867c02a8873fd09c50b920adb5058d8656
Aasir Mutwalli <aasirmutwalli22@gmail.com>
2022-02-11 09:13:47 +0530

    upgrading http_get_request code

--- http_get_request.py
+++ http_get_request.py
@@ -4,10 +4,14 @@ url = "http://maps.googleapis.com/maps/api/geocode/json"
 location = "delhi technological university"
 
 params = {'address':location}
-
+# get request
 response = requests.get(url = url, params = params)
-
+# post request
+# response = requests.post(url = url, params = params)
+# response as json
 data = response.json()
+# response as text
+# text = response.text
 
 results = data['results']
 error = data['error_message']
