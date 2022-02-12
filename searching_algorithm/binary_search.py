ef618370a28330d5029fe3618f747fc975c3f330
aasirmutwalli22 <aasirmutwalli22@gmail.com>
2022-02-12 22:10:11 +0530

    binary_search algorithm implimented

--- searching_algorithm/binary_search.py
+++ searching_algorithm/binary_search.py
@@ -1,19 +1,19 @@
 # array should be compulsary sorted to used binary search algorithm
 # https://www.geeksforgeeks.org/binary-search/
-myList = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-myNum = 18
+myList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+myNum = 11
 
 
 def binarySearch(left, right):
-    if right >= left:  # element may be present
-        middleIndex = left + (right-1) // 2
-        if myNum == myList[middleIndex] :
+    if right >= left:
+        middleIndex = (right + left) // 2
+        if myNum == myList[middleIndex]:
             return middleIndex
-        elif myList[middleIndex] > myNum :
-            return binarySearch(left, middleIndex - 1)
+        elif myNum > myList[middleIndex]:
+            return binarySearch(middleIndex + 1, right)
         else:
-            return binarySearch(middleIndex+1, right)
-    else:  # empty array element not present
+            return binarySearch(left, middleIndex - 1)
+    else:
         return -1
 
 index = binarySearch(0, len(myList) - 1)
